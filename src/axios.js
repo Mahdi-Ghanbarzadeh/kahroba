ce316c173d0d841c7b577759126275d79fdab1a1
Mahdi-Ghanbarzadeh <mahdi.ghanbarzadeh.a@gmail.com>
2023-01-25 13:47:33 +0330

    change baseUrl

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const baseUrl = "http://internetengineering.pythonanywhere.com/";
-const baseUrl = "https://api.feshin.me/";
+const baseUrl = "https://feshin.me/";
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
